d0d2d03b9f6cdcb8ede5c61995b02e55fce6bf36
Marwa <marabohaidar@gmail.com>
2023-11-09 23:49:16 +0200

    create user homepage

---
+++ index.js
@@ -0,0 +1,25 @@
+function showMenu(icon) {
+    // Hide all other menus
+    hideAllMenus();
+
+    // Find the parent menu and show its actions-menu
+    const parentMenu = icon.closest('.post_menu');
+    const actionsMenu = parentMenu.querySelector('.actions-menu');
+    actionsMenu.style.display = 'block';
+
+    // Close the menu when clicking outside of it
+    document.addEventListener('click', function closeMenu(event) {
+        if (!parentMenu.contains(event.target)) {
+            actionsMenu.style.display = 'none';
+            document.removeEventListener('click', closeMenu);
+        }
+    });
+}
+
+function hideAllMenus() {
+    // Hide all actions-menus
+    const allMenus = document.querySelectorAll('.actions-menu');
+    allMenus.forEach(menu => {
+        menu.style.display = 'none';
+    });
+}

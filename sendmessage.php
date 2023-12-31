0ff11d456a970fc5d4428ea57039612b6463bcfc
Marwa <marabohaidar@gmail.com>
2023-12-31 12:25:04 +0200

    contactus

--- sendmessage.php
+++ sendmessage.php
@@ -12,6 +12,13 @@ if(isset($_POST['send_message'])){
     $query->bind_param("is", $userId, $message);
     $query->execute();
     $query->close();
+    
+    // Set a session variable to indicate successful message submission
+    // $_SESSION['message_sent'] = true;
+    // Redirect back to the "Contact Us" page
+    header("Location: contactus.php");
+    exit(); // Ensure that the script stops executing after the header is sent
+
 }
 
 
